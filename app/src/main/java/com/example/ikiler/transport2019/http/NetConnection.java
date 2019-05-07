7e869bd0b30b20c6a43b8e83c3e045cd9e07f3f7
ikiler <ikiler@126.com>
2019-05-07 16:15:53 +0800

    init

--- app/src/main/java/com/example/ikiler/transport2019/http/NetConnection.java
+++ app/src/main/java/com/example/ikiler/transport2019/http/NetConnection.java
@@ -1,8 +1,12 @@
 package com.example.ikiler.transport2019.http;
 
 import android.arch.lifecycle.Lifecycle;
+import android.os.Handler;
+import android.os.Looper;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -18,30 +22,32 @@ import okio.BufferedSink;
 public class NetConnection {
 
     static OkHttpClient client = new OkHttpClient();
+    static Handler handler = new Handler(Looper.getMainLooper());
 
-    public static void post(String url, String json, final CallBack callBack){
-
-        final Request request2 = new Request.Builder()
-                .url(url)
-                .post(RequestBody.create(MediaType.parse("application/json"),json))
-                .build();
+    public static void post(final List<String> urls, final String json, final CallBack callBack){
+        final List<String> jsons = new ArrayList<>();
         new Thread(new Runnable() {
             @Override
             public void run() {
-                try {
-                    Response response = client.newCall(request2).execute();
-                    if (response.isSuccessful()){
-                        callBack.success(response.body().string());
+                for (String url:urls){
+                    final Request request2 = new Request.Builder()
+                            .url(url)
+                            .post(RequestBody.create(MediaType.parse("application/json"),json))
+                            .build();
+                    try {
+                        Response response = client.newCall(request2).execute();
+                        if (response.isSuccessful()){
+                            jsons.add(response.body().string());
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
+                callBack.success(jsons);
             }
         }).start();
-
-
     }
     public interface CallBack{
-        void success(String result);
+        void success(List<String> results);
     }
 }
